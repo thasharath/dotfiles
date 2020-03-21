bba93c8b017bedb4c9b16fefbe9b8983dd3a1d10
Thasharath Somasegaram <thasharath@zoho.com>
2020-03-21 15:58:37 +0530

    added d_menu

---
+++ Documents/appimage/dmenu-4.9/config.h
@@ -0,0 +1,33 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 900;                    /* minimum width when centered */
+
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=18"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#232323" },
+	[SchemeSel] = { "#eeeeee", "#1f697c" },
+	[SchemeSelHighlight] = { "#91ff5a", "#1f697c" },
+	[SchemeNormHighlight] = { "#91ff5a", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeMid] = { "#eeeeee", "#222222" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 3;
+static unsigned int lineheight = 4;         /* -h option; minimum height of a menu line     */
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 3;
